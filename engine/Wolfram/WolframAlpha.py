177cb1cfb405aede463212a311f6bbb7f4480a04
Toby Clark <psytc9@nottingham.ac.uk>
2024-11-09 15:30:54 +0000

    add: get answer

--- engine/Wolfram/WolframAlpha.py
+++ engine/Wolfram/WolframAlpha.py
@@ -3,25 +3,11 @@ from Keys import WOLFRAM_ALPHA_APPID
 import requests
 import urllib.parse
 
-appid = WOLFRAM_ALPHA_APPID
 
-# Your query
-query = urllib.parse.quote_plus("5 + 6 + 7^2 =")
-
-# Construct the query URL with the dynamic query
-query_url = f"http://api.wolframalpha.com/v2/query?appid={appid}&input={query}&output=JSON"
-
-# Send the request to the Wolfram Alpha API
-r = requests.get(query_url)
-
-# Parse the JSON response
-response = r.json()
-
-# Extract the result from the response
-result_pod = next(pod for pod in response['queryresult']['pods'] if pod['id'] == 'Result')
-
-# Get the result (plaintext value)
-result = result_pod['subpods'][0]['plaintext']
-
-# Print the result
-print(f"Result: {result}")
+def get_answer(latex_str: str) -> str:
+    # Your query
+    appid = WOLFRAM_ALPHA_APPID
+    query = urllib.parse.quote_plus("\\sum_{i=0}^{100} i")
+    query_url = f"http://api.wolframalpha.com/v1/result?appid={appid}&input={query}&output=JSON"
+    r = requests.get(query_url)
+    pprint(r.json())
