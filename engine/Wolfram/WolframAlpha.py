1e15fb0bd6c45219acf9d203206df12e423eea34
Toby Clark <psytc9@nottingham.ac.uk>
2024-11-09 15:57:01 +0000

    add: wolfram

--- engine/Wolfram/WolframAlpha.py
+++ engine/Wolfram/WolframAlpha.py
@@ -7,8 +7,9 @@ import urllib.parse
 def get_answer(latex_str: str) -> str:
     # Your query
     appid = WOLFRAM_ALPHA_APPID
-    query = urllib.parse.quote_plus("\\sum_{i=0}^{100} i")
+    query = urllib.parse.quote_plus("x + 5 = 10\n x = ?")
     query_url = f"http://api.wolframalpha.com/v1/result?appid={appid}&input={query}&output=JSON"
     r = requests.get(query_url)
-    pprint(r.json())
     return str(r.text)
+
+print(get_answer("a"))
